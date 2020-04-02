ded346fd1f0ba8d55c4169aa2b2db916791d5c0e
wangzaiaaa <2627894288@qq.com>
2020-04-02 12:21:05 +0800

    包含min函数的栈30

---
+++ src/leetcode/包含min函数的栈30.java
@@ -0,0 +1,39 @@
+package leetcode;
+
+import java.util.Stack;
+
+public class 包含min函数的栈30 {
+    Stack<Integer> stack = new Stack<>();
+    Stack<Integer> min = new Stack<>();
+    /** initialize your data structure here. */
+
+    public void push(int x) {
+        stack.push(x);
+        if(min.isEmpty() || x < min.peek()){
+            min.push(x);
+        }else{
+            min.push(min.peek());
+        }
+    }
+
+    public void pop() {
+        if(!stack.isEmpty()){
+            stack.pop();
+            min.pop();
+        }
+    }
+
+    public int top() {
+        if(!stack.isEmpty()){
+            return stack.peek();
+        }
+        return -1;
+    }
+
+    public int min() {
+        if(!min.isEmpty()){
+            return min.peek();
+        }
+        return -1;
+    }
+}
