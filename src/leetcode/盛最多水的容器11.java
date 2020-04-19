4bd07ee3dc92dbbad2c06c4923fb954edf0862e2
wangzaiaaa <2627894288@qq.com>
2020-04-19 08:42:38 +0800

    整数反转7

--- src/leetcode/盛最多水的容器11.java
+++ src/leetcode/盛最多水的容器11.java
@@ -13,4 +13,26 @@ public class 盛最多水的容器11 {
         }
         return res;
     }
+    public boolean isPalindrome(String s) {
+        if(s == null || s.length() == 0){
+            return false;
+        }
+        StringBuilder sb = new StringBuilder();
+        char c;
+        for(int i = 0; i < s.length(); i++){
+            c = s.charAt(i);
+            if(check(c)){
+                sb.append(c);
+            }
+        }
+        String str = sb.toString();
+        String rev = sb.reverse().toString();
+        return str.equalsIgnoreCase(rev);
+    }
+    public boolean check(char c){
+        if((c >='0' && c <= '9') || (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')){
+            return true;
+        }
+        return false;
+    }
 }
