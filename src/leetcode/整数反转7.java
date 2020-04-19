018c81a5912921153e478ecd6ed45d9140477409
wangzaiaaa <2627894288@qq.com>
2020-04-19 08:43:49 +0800

    ...

--- src/leetcode/整数反转7.java
+++ src/leetcode/整数反转7.java
@@ -3,14 +3,13 @@ package leetcode;
 public class 整数反转7 {
     public int reverse(int x) {
         int max = Integer.MAX_VALUE,min = Integer.MIN_VALUE;
-        public int reverse(int x) {
-            long res = 0;
-            while(x != 0){
-                res = res * 10 + x % 10;
-                x /= 10;
-                if(res < min || res > max) return 0;
-            }
-            return (int)res;
+        long res = 0;
+        while(x != 0){
+            res = res * 10 + x % 10;
+            x /= 10;
+            if(res < min || res > max) return 0;
         }
+        return (int)res;
+
     }
 }
